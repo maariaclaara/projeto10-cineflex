c0f66920b30554d8107c10d75d484decd39d3e70
Maria Clara <129561798+maariaclaara@users.noreply.github.com>
2023-05-29 15:06:50 -0300

    AVALIAR NO HUB

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,4 @@
-axios.defaults.headers.common['Authorization'] = 'dqsyLkH9DHBubpqTeI3b2I7Q';
-
 import React from 'react'
-import axios from "axios";
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import GlobalStyle from './style/GlobalStyle.jsx'
